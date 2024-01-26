1d88fb23cfb65d66c819cff6cd874642b6628a52
Egor Ponomarev <e.ponomaryow2017@yandex.ru>
2024-01-26 09:38:28 +0300

    - Исправил баг с тем, что нельзя было отменить сохранение кружочка

--- src/main/kotlin/features/download_voice/oga/CancelSaving.kt
+++ src/main/kotlin/features/download_voice/oga/CancelSaving.kt
@@ -20,14 +20,31 @@ class CancelSaving : Chain(OnCallbackDataGotten("cancelSaving")) {
     override suspend fun executableChain(updating: Updating): List<Executable> {
         val voiceId = updating.map(UpdatingCallbackInt("cancelSaving"))
         return try {
-            if (voiceId != -1) VoiceStorage.Base.Instance().voiceById(voiceId.toLong())
-            listOf(
-                AnswerToCallback(
-                    mKey,
-                    ContextString.Base.Strings().string(sVoiceAlreadySaved, updating),
-                    true
+            if (voiceId != -1) {
+                VoiceStorage.Base.Instance().voiceById(voiceId.toLong())
+                listOf(
+                    AnswerToCallback(
+                        mKey,
+                        ContextString.Base.Strings().string(sVoiceAlreadySaved, updating),
+                        true
+                    )
                 )
-            )
+            } else {
+                mStates.state(updating).editor(mStates).apply {
+                    deleteValue("waitForTitle")
+                    deleteValue("isAudio")
+                    deleteValue("videoNoteId")
+                    deleteValue("videoDuration")
+                }.commit()
+                listOf(
+                    AnswerToCallback(
+                        mKey,
+                        ContextString.Base.Strings().string(sVoiceSavingCanceled, updating),
+                        true
+                    ),
+                    DeleteMessage(mKey, updating),
+                )
+            }
         } catch (e: VoiceNotFound) {
             mStates.state(updating).editor(mStates).apply {
                 deleteValue("waitForTitle")
