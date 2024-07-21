a71336efb865c1df3320046b49f8b365536aa5ec
Egor Ponomarev <e.ponomaryow2017@yandex.ru>
2024-07-21 22:05:01 +0300

    - Добавлена обработка ошибки когда файл слишком большой и сообщение для юзера об этом

--- src/main/kotlin/features/download_voice/ChainForCatchMedia.kt
+++ src/main/kotlin/features/download_voice/ChainForCatchMedia.kt
@@ -6,11 +6,14 @@ import core.Updating
 import domain.logExceptionToAll
 import domain.messages.ContactDevMessage
 import executables.Executable
+import executables.SendMessage
 import handlers.BotRecognizerEvent
 import handlers.UnhandledEvent
 import helpers.FileDownloadException
 import logs.Logging
 import org.json.JSONObject
+import sVoiceIsTooBig
+import translations.domain.ContextString
 
 class ChainForCatchMedia(
     private val sourceChain: Chain
@@ -29,8 +32,17 @@ class ChainForCatchMedia(
         return try {
             sourceChain.executableChain(updating)
         } catch (e: FileIdFailed) {
-            Logging.ConsoleLog.logExceptionToAll(e)
-            listOf(ContactDevMessage(mKey, updating))
+            if (e.errorMessage == "Bad Request: file is too big") {
+                listOf(
+                    SendMessage(
+                        mKey,
+                        ContextString.Base.Strings().string(sVoiceIsTooBig, updating)
+                    )
+                )
+            } else {
+                Logging.ConsoleLog.logExceptionToAll(e)
+                listOf(ContactDevMessage(mKey, updating))
+            }
         } catch (e: FileDownloadException) {
             Logging.ConsoleLog.logExceptionToAll(e)
             listOf(ContactDevMessage(mKey, updating))
